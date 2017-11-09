a00016625f933313c8d3534531a8f191408727e7
JelteMX <jelte.lagendijk@mendix.com>
2017-11-09 13:29:49 +0100

    Styling modifications & remove title from links

--- src/app/Header/HeaderLink.vue
+++ src/app/Header/HeaderLink.vue
@@ -1,14 +1,14 @@
 <template>
   <div :class="b()" @mouseenter="mouseenter" @mouseleave="mouseleave" @click="menu">
-    <a :class="b('head', { on, 'has-sub': link.sub && link.sub.length > 0 })" v-if="link.url" :href="link.url" :title="link.title">{{ link.title }}</a>
-    <a :class="b('head', { on, 'has-sub': link.sub && link.sub.length > 0 })" v-else href="#" :title="link.title" @click.stop.prevent="menu">{{ link.title }}</a>
+    <a :class="b('head', { on, 'has-sub': link.sub && link.sub.length > 0 })" v-if="link.url" :href="link.url">{{ link.title }}</a>
+    <a :class="b('head', { on, 'has-sub': link.sub && link.sub.length > 0 })" v-else href="#" @click.stop.prevent="menu">{{ link.title }}</a>
     <span :class="b('expand')" v-if="link.sub && link.sub.length > 0" @click.stop.prevent="menu">
       <span :class="b('expand-icon', { 'active': on })"></span>
     </span>
     <div :class="b('submenu', { on })" v-if="link.sub && link.sub.length > 0">
       <ul :class="b('linkblock')">
         <li :class="b('link')" v-for="(sub, index) in link.sub" :key="index">
-          <a :href="sub.url" :title="sub.title">{{ sub.title }}</a>
+          <a :href="sub.url">{{ sub.title }}</a>
         </li>
       </ul>
     </div>
