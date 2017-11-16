db7aaf741c6542f4e3964e3d5fbc0ef8696ce90e
JelteMX <jelte.lagendijk@mendix.com>
2017-11-16 14:18:22 +0100

    Link & mobile fixes

--- src/app/Footer/LinkBlock.vue
+++ src/app/Footer/LinkBlock.vue
@@ -1,6 +1,7 @@
 <template>
   <div :class="b({'footer': true})">
-    <span :class="b('title')" v-if="link.title">{{ link.title }}</span>
+    <span :class="b('title')" v-if="link.title && !link.url">{{ link.title }}</span>
+    <a :class="b('title')" v-else-if="link.title && link.url" :href="link.url">{{ link.title }}</a>
     <ul :class="b('links', {'no-title': !link.title})" v-if="link.sub && link.sub.length > 0">
       <li :class="b('link', {'type': sub.type})" v-for="(sub, index) in link.sub" :key="index">
         <a :href="sub.url" v-if="!sub.external">{{ sub.title }}</a>
--- src/app/Header/HeaderLink.vue
+++ src/app/Header/HeaderLink.vue
@@ -1,11 +1,10 @@
 <template>
-  <div :class="b()" @mouseenter="mouseenter" @mouseleave="mouseleave">
-    <a :class="b('head', { on, 'has-sub': link.sub && link.sub.length > 0 })" v-if="link.url" :href="link.url" @click="topmenu">{{ link.title }}</a>
-    <a :class="b('head', { on, 'has-sub': link.sub && link.sub.length > 0 })" v-else href="#" @click.stop.prevent="menu">{{ link.title }}</a>
+  <div :class="b({'has-sub': link.sub && link.sub.length > 0})" @mouseenter="mouseenter" @mouseleave="mouseleave">
+    <toplink :on="on" :link="link" :mob="mob" :menu="menu" :topmenu="topmenu" />
     <span :class="b('expand')" v-if="link.sub && link.sub.length > 0" @click.stop.prevent="menu">
       <span :class="b('expand-icon', { 'active': on })"></span>
     </span>
-    <div :class="b('submenu', { on })" v-if="link.sub && link.sub.length > 0">
+    <div :class="b('submenu', { on })" v-if="link.sub && link.sub.length > 0" :style="styleHeight">
       <ul :class="b('linkblock')">
         <li :class="b('link')" v-for="(sub, index) in link.sub" :key="index">
           <a :href="sub.url" v-if="!sub.external">{{ sub.title }}</a>
@@ -17,6 +16,7 @@
 </template>
 <script>
 import Vue from 'vue';
+import toplink from './Toplink.vue';
 
 export default {
   name: 'headerlink',
@@ -25,6 +25,22 @@ export default {
     return {
       on: false
     }
+  },
+  computed: {
+    styleHeight: function() {
+      if (!this.mob) {
+        return '';
+      } else if (!this.on) {
+        return 'max-height: 0px;';
+      } else if (!this.link.sub || this.link.sub.length === 0) {
+        return 'max-height: 0px';
+      } else {
+        return `max-height: calc(${(this.link.sub.length * 32)}px + 1em)`;
+      }
+    }
+  },
+  components: {
+    toplink,
   },
   methods: {
     menu() {
