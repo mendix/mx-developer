e1ef452ab870bf31f22f7c8b1b4ade9d104d78f2
JelteMX <jelte.lagendijk@mendix.com>
2017-11-16 14:18:39 +0100

    Add Headerlink component

---
+++ src/app/Header/TopLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-if="link.url && !link.external" :href="link.url" @click="menuClick">{{ link.title }}</a>
+    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-else-if="link.url && link.external && link.sub && link.sub.length > 0" :href="link.url" @click="menuClick" target="_blank" rel="noopener">{{ link.title }}</a>
+    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-else-if="link.url && link.external && !(link.sub && link.sub.length > 0)" :href="link.url" target="_blank" rel="noopener">{{ link.title }}</a>
+    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-else href="#" @click.stop.prevent="menu">{{ link.title }}</a>
+</template>
+<script>
+import Vue from 'vue';
+
+export default {
+  name: 'headerlink__head',
+  props: ['link', 'mob', 'on', 'menu', 'topmenu'],
+  methods: {
+    menuClick(evt) {
+      if (this.link.sub.length === 0) {
+        return;
+      }
+      this.topmenu(evt);
+    }
+  }
+};
+</script>
