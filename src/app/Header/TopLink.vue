7a5f17f20ed0c4fb7c38a44a1e7ad2fa09c89ec2
Jelte Lagendijk <JelteMX@users.noreply.github.com>
2017-11-22 09:12:13 +0100

    Redesign (#1)
    * Community adjustments
    
    * Footer links adjustments
    
    * Add CSS

--- src/app/Header/TopLink.vue
+++ src/app/Header/TopLink.vue
@@ -1,8 +1,8 @@
 <template>
-    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-if="link.url && !link.external" :href="link.url" @click="menuClick">{{ link.title }}</a>
-    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-else-if="link.url && link.external && link.sub && link.sub.length > 0" :href="link.url" @click="menuClick" target="_blank" rel="noopener">{{ link.title }}</a>
-    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-else-if="link.url && link.external && !(link.sub && link.sub.length > 0)" :href="link.url" target="_blank" rel="noopener">{{ link.title }}</a>
-    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-else href="#" @click.stop.prevent="menu">{{ link.title }}</a>
+    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0, 'is-link': true })" v-if="link.url && !link.external" :href="link.url" @click="menuClick">{{ link.title }}</a>
+    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0, 'is-link': true })" v-else-if="link.url && link.external && link.sub && link.sub.length > 0" :href="link.url" @click="menuClick" target="_blank" rel="noopener">{{ link.title }}</a>
+    <a :class="b({ on, 'has-sub': link.sub && link.sub.length > 0, 'is-link': true })" v-else-if="link.url && link.external && !(link.sub && link.sub.length > 0)" :href="link.url" target="_blank" rel="noopener">{{ link.title }}</a>
+    <span :class="b({ on, 'has-sub': link.sub && link.sub.length > 0 })" v-else @click.stop.prevent="menu">{{ link.title }}</span>
 </template>
 <script>
 import Vue from 'vue';
