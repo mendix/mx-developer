e61615b9faa671b7f65eec354f72ce6556d6f02a
JelteMX <jelte.lagendijk@mendix.com>
2018-01-26 15:05:00 +0100

    Fix tracker

--- src/app/store/actions.js
+++ src/app/store/actions.js
@@ -1,5 +1,7 @@
 import fetchJsonp from 'fetch-jsonp';
-import {trackShow, trackPage, trackEvent} from './tracker';
+import {trackShow, trackPage, trackEvent, outBoundLinkTracker} from './tracker';
+
+window.__mxOutBoundLinkTracker = outBoundLinkTracker;
 
 const profileUrl = `https://home.mendix.com/mxid/appbar2?q=${Number(new Date())}`;
 const isPartnerUrl = 'https://developer.mendixcloud.com/rest/checkpartner?openid=';
