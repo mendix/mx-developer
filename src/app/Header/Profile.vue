4d68a301c4f02e5c56e4ed4191cc6569dd2704f3
Jelte Lagendijk <JelteMX@users.noreply.github.com>
2018-03-15 11:11:30 +0100

    Update Profile.vue

--- src/app/Header/Profile.vue
+++ src/app/Header/Profile.vue
@@ -14,7 +14,7 @@
       <div :class="b('submenu', { open })">
         <span :class="b('display-name')">Welcome, {{ profile && profile.displayName }}</span>
         <a :class="b('submenu__link', { 'type': 'platform' })" :href="urls.platform" v-track-link>Developer Portal</a>
-        <a :class="b('submenu__link', { 'type': 'community' })" :href="urls.community" v-if="profile" v-track-link>My Profile</a>
+        <a :class="b('submenu__link', { 'type': 'community' })" :href="urls.community" v-if="profile" v-track-link>My Dashboard</a>
         <a :class="b('submenu__link', { 'type': 'developer' })" :href="urls.developer" v-if="profile" v-track-link>Account Settings</a>
         <a :class="b('submenu__link', { 'type': 'logout' })" :href="profile.logoutUrl" v-if="profile && profile.logoutUrl" v-track-link>Log out</a>
       </div>
