fbb8aa895cd969e4cb7487118444c5e2fe736d46
JelteMX <jelte.lagendijk@mendix.com>
2018-06-28 16:00:33 +0200

    Color fixes

--- src/app/Header/Profile.vue
+++ src/app/Header/Profile.vue
@@ -20,7 +20,7 @@
         <a :class="b('submenu__link', { 'type': 'platform' })" :href="homeURL" @click.stop="home($event)" id="mx-header-link-devportal" v-track-link>Developer Portal</a>
         <a :class="b('submenu__link', { 'type': 'community' })" :href="urls.community" v-if="profile" id="mx-header-link-dashboard" v-track-link>My Dashboard</a>
         <a :class="b('submenu__link', { 'type': 'developer' })" :href="urls.developer" @click.stop="openProfile($event)" v-if="profile" id="mx-header-link-accountsettings" v-track-link>Account Settings</a>
-        <admin-links v-if="profile" :item-class="b('submenu__link', { 'type': 'developer' })" :closeFunc="closeMenu" />
+        <admin-links v-if="profile" :item-class="b('submenu__link', { 'type': 'admin' })" :closeFunc="closeMenu" />
         <a :class="b('submenu__link', { 'type': 'logout' })" :href="logoutLink" v-if="profile && profile.logoutUrl" v-track-link id="mx-header-link-logout">Log out</a>
       </div>
     </div>
