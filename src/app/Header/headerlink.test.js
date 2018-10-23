8c34768002ff82f2825c912cf095e4ffa21e3cd8
Stanislau Yudzitski <stanislauy@playtika.com>
2018-10-23 13:44:21 +0300

    Setting up Jest testing environment. Smoke test on HeaderLink component.

---
+++ src/app/Header/headerlink.test.js
@@ -0,0 +1,7 @@
+import HeaderLink from "./HeaderLink.vue";
+
+describe('HeaderLink component', () => {
+  it('Should return proper component name', () => {
+    expect(HeaderLink.name).toBe('headerlink');
+  });
+});
