9804292e5cd45b698a57d1e4dee7103b926adb5f
Huang-Ming Chang <huang.ming.chang@mendix.com>
2019-08-28 00:56:39 +0200

    bundle in one file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        'bundle.js': glob
+            .sync('build/static/?(js|css)/main.*.?(js|css)')
+            .map(f => path.resolve(__dirname, f)),
+    },
+    output: {
+        filename: 'build/static/js/bundle.min.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [new UglifyJsPlugin()],
+};
