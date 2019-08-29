763a9313b9c9f7eafcc585b515b2a7a070c36e85
Huang-Ming Chang <huang.m.chang@gmail.com>
2019-08-29 18:50:46 +0200

    use observer to monitor DOM

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import { waitForElementId } from './utils/mxHelpers';
 import MxHeader from './components/MxHeader';
 import MxFooter from './components/MxFooter';
 
 import './style/MxHeader.scss';
 import './style/MxFooter.scss';
 
-// waitForElementId('mxHeader', MxHeader);
-// waitForElementId('mxFooter', MxFooter);
+const observer = new MutationObserver(() => {
+    const headerElements = document.getElementsByClassName('mxHeader');
+    const footerElements = document.getElementsByClassName('mxFooter');
 
-ReactDOM.render(<MxHeader />, document.getElementById('mxHeader'));
-ReactDOM.render(<MxFooter />, document.getElementById('mxFooter'));
+    if (headerElements.length > 0 && footerElements.length > 0) {
+        ReactDOM.render(<MxHeader />, headerElements[0]);
+        ReactDOM.render(<MxFooter />, footerElements[0]);
+
+        observer.disconnect();
+    }
+});
+
+observer.observe(document.body, { childList: true });
+
+/**
+ * Testing locally
+ */
+
+setTimeout(function() {
+    const headerElement = document.createElement('div');
+    const contentElement = document.createElement('div');
+    const footerElement = document.createElement('div');
+    headerElement.className = 'mxHeader';
+    contentElement.setAttribute('style', 'height: 800px; width: 100%');
+    footerElement.className = 'mxFooter';
+    document.body.appendChild(headerElement);
+    document.body.appendChild(contentElement);
+    document.body.appendChild(footerElement);
+}, 100);
