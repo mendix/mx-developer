b9a250107c1aede1074fcf09392e721e063596f6
Huang-Ming Chang <huang.m.chang@gmail.com>
2019-08-30 16:23:44 +0200

    Add test for MxFooter

---
+++ src/components/MxFooter/__tests__/MxFooter.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Footer from '../index';
+
+describe('MxFooter', () => {
+    it('should render correctly', () => {
+        const { container } = render(<Footer />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+});
