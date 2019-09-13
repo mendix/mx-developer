ac86fca5d17aa07e9c33812805e3cc138d398c1c
Huang-Ming Chang <huang.m.chang@gmail.com>
2019-09-13 14:00:16 +0200

    Added error handler when no css classes are found

--- src/index.tsx
+++ src/index.tsx
@@ -9,13 +9,21 @@ import observe from './utils/observe';
 import './style/MxHeader.scss';
 import './style/MxFooter.scss';
 
-let header: Element;
-let footer: Element;
+let header: Element | undefined;
+let footer: Element | undefined;
 
 const mount = (className: string, Component: React.ComponentType<any>) => {
-    const element = document.getElementsByClassName(className)[0];
-    if (element) ReactDOM.render(<Component />, element);
-    return element;
+    try {
+        const element = document.getElementsByClassName(className)[0];
+        if (element) ReactDOM.render(<Component />, element);
+        return element;
+    } catch (error) {
+        console.error(`
+Cannot find any tag with the specific css classes. Please do the following:
+-   Create a container with css class 'mxHeader' at the top of your layout.
+-   Create a container with css class 'mxFooter' at the bottom of your layout.
+        `);
+    }
 };
 observe(observer => {
     header = mount('mxHeader', MxHeader);
